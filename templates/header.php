ba492b9ba2f84ae23f56d9646839de68fe15c424
jonas-amilton <jonas96@edu.unisinos.br>
2024-02-12 18:30:50 -0300

    [refactor] organizando arquitetura do projeto

---
+++ templates/header.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once('config/url.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agendas de Contatos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <script defer src="https://kit.fontawesome.com/fc52766eca.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a href="<?= $BASE_URL ?>index.php" class="nav-link active" id="home-link">Agenda</a>
+                    <a href="<?= $BASE_URL ?>create.php" class="nav-link active" id="home-link">Adicionar Contato</a>
+                </div>
+            </div>
+        </nav>
+    </header>
